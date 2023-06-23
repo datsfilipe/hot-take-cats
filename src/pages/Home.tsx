a9d5325cd7803c60d9ddf29a1973f6ee385101ca
datsfilipe <datsfilipe.foss@proton.me>
2023-06-23 09:45:28 -0300

    feat: add navigation when right password is entered

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "@tanstack/router";
 
 export const Home = () => {
+  const navigate = useNavigate({ from: "/" });
+
   const [password, setPassword] = useState<string | null>(null);
   const numbers = Array.from(Array(9), (_, i) => i + 1);
   numbers.push(0);
+
+  useEffect(() => {
+    if (password === "666") {
+      alert("You have unlocked the secret!");
+
+      setTimeout(() => {
+        navigate({
+          to: "/cat",
+        });
+      }, 1500);
+    }
+  }, [password, navigate]);
 
   return (
     <div>
