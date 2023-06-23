6cac8ea978487ae4cff5c0673b9eebc39dd51aad
datsfilipe <datsfilipe.foss@proton.me>
2023-06-23 12:23:19 -0300

    feat(cat): add cat page

--- src/pages/Cat.tsx
+++ src/pages/Cat.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import phrases from "../utils/phrases.json";
+
 export const Cat = () => {
+  const [phrase, setPhrase] = useState<string>();
+
+  useEffect(() => {
+    const random = Math.floor(Math.random() * phrases.length);
+
+    const phrase = encodeURIComponent(phrases[random]);
+
+    setPhrase(phrase);
+  }, []);
+
   return (
-    <>
-      <h1>Cat</h1>
-      <p>This is the cats page.</p>
-    </>
+    <div className="w-full h-full flex justify-center items-center">
+      <img
+        src={`https://cataas.com/cat/says/${phrase}`}
+        alt={`Cat image with "${phrase}" phrase written.`}
+      />
+    </div>
   );
 };
